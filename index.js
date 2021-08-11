380b711b2e8a3873aaf150b62e7e42e26d0d954c
abibflores <36962653+abibflores@users.noreply.github.com>
2021-08-11 16:08:39 -0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Home = () => (
+    <>
+        <h1>Hola mundo desde amplify</h1>
+        <Image width="720" height="405" src="/image/logo.png" alt="Picture of the author" />
+    </>
+)
+
+export default Home
