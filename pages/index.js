5136ccba8a11e1cdfd2a5d41ce07d0c332983c63
abibflores <abib.m@reworth.co>
2021-10-28 16:09:08 -0500

    feat(TemplateGithub): the text in index page is changed

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const urlBase = process.env.NEXT_PUBLIC_ROOT_DEV;
 
 const Home = () => (
     <>
-        <h1>Hola mundo desde amplify {urlBase}</h1>
+        <h1>Hola mundo desde amplify {urlBase} with Github template</h1>
         <Image width="720" height="405" src="/image/logo.png" alt="Picture of the author" />
     </>
 )
